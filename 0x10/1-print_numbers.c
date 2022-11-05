0573d4eb6fda44b827c7b272c7d0700289d87ffe
Tednoob17 <teddams047@gmail.com>
2022-11-05 04:38:23 +0100

    1 begin

---
+++ 0x10/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - this functions prints numbers followed by a new line
+ *@separator: this a strings who separate any numbers in them
+ *@n: number of integers passed in arguments
+ @Return : Void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+
+
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
+}

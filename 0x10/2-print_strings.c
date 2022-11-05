0a065f237af132176f35ae5c6732926b9ecc203e
Tednoob17 <teddams047@gmail.com>
2022-11-05 22:21:56 +0100

    pokkk

--- 0x10/2-print_strings.c
+++ 0x10/2-print_strings.c
@@ -1,4 +1,5 @@
-include "variadic_functions.h"
+#include "variadic_functions.h"
+#include <stdarg.h>
 /**
  *print_strings - this functions prints strings followed by the new line
  *@separator: it's strings who separate any strings into them
@@ -7,11 +8,22 @@ include "variadic_functions.h"
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i = 0;
+	va_list arg;
 
-
-
-
-
+	va_start(arg, n);
+	while(i < n)
+	{
+		if (i == 0)
+			printf("%s",va_arg(arg, char *));
+		else
+		{
+			printf("%s",separator);
+			printf("%s",va_arg(arg, char *));
+		}
+		i++;
+	}
+	va_end(arg);
 }
 /**
  * main - check the code

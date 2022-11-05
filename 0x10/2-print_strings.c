7ebd0689e8aa55dd2277f21d65c051392432b98f
Tednoob17 <teddams047@gmail.com>
2022-11-05 19:21:12 +0100

    Ethic?

---
+++ 0x10/2-print_strings.c
@@ -0,0 +1,25 @@
+include "variadic_functions.h"
+/**
+ *print_strings - this functions prints strings followed by the new line
+ *@separator: it's strings who separate any strings into them
+ *@n: it's numbers of variable in parameteers
+ *Return: Void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+
+
+
+
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
+}

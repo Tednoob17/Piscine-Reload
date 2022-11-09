578d61107405198e105e2cd81b3a77f5a53620da
Tednoob17 <teddams047@gmail.com>
2022-11-09 17:52:05 +0100

    bye

--- 0x10/3-print_all.c
+++ 0x10/3-print_all.c
@@ -1,5 +1,73 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+
+
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format,... );
+
+/**
+ * print_char - print char is it's char who is an parameters
+ */
+void print_char(va_list arg){
+	return(printf("%c",c));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
 *print_all - this print all
 *@format: is a list of types of arguments passed to the functions
@@ -10,18 +78,23 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	va_list arg;
 
-	if (!format)	
+	if (!format)
 		printf("(nil)");
 
 	va_start(arg, format);
 	while (*(format + i) != '\0')
 	{
+		if (*(format + i) == c || *(format + i) == i || *(format + i) == f || *(format + i) == s )
+		{
+			    
 		
-	
-	
-	
+		}
+		
+		
+		
 	}
-
+	va_end(arg);
+	printf("\n");
 }
 
 
@@ -31,7 +104,8 @@ void print_all(const char * const format, ...)
  * Return: Always 0.
  */
 int main(void)
-{void print_all(const char * const format, ...);
+{
+    void print_all(const char * const format, ...);
     print_all("ceis", 'B', 3, "stSchool");
     return (0);
 }

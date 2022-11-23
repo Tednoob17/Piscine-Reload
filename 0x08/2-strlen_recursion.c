e7ef51e28bd97ba069fa939fd9d0ac8896df14b1
Tednoob17 <teddams047@gmail.com>
2022-11-23 19:20:16 +0100

    ran

---
+++ 0x08/2-strlen_recursion.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_strlen_recursion - print size of string
+ *@s: string
+ *Return : Size of string
+ */
+int _strlen_recursion(char *s)
+{
+
+	int n = 0;
+
+	if (*s != '\0')
+	{
+	      n +=_strlen_recursion(s + 1) + 1 ;
+	}
+return (n);
+}
+
+
+
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}

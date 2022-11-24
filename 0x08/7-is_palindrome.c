5a2f22508a88059c996d42a02d05685a245ecbba
Tednoob17 <teddams047@gmail.com>
2022-11-24 19:42:28 +0100

    re-luck 3

--- 0x08/7-is_palindrome.c
+++ 0x08/7-is_palindrome.c
@@ -21,40 +21,13 @@ int main(void)
     return (0);
 }
 int is_palindrome(char *s){
-	return(check_it(s));
-	
-}
-int check_it(char *s){
-	int j = 0, i = 0, n = 0, first = 0,end = 0, v, m, k;
-	/*char *first = "", end = "";*/
-	if (!(*s)){
-		return(0);
-	}
+	int i = 0;
 
 	while (*(s + i) != '\0'){
-		first = *(s + i);
- 		n++;
- 		i++;
-		}
-		if (i % 2 != 0)
-			return(0);
-
-		j = i;
-		end = *(s + j);
-		i = 0;
-		v = j / 2;
-		while (i < (j / 2)){
-			if (first == end){
-				first = *(s + i);
-				m = j - i;
-				end = *(s - m);
-				k++;
-			}
-			i++;
-		}
-		if ( v == k)
-			return(0);
-		else
-			return(1);
+		i++;
+	}
+	
+	return(i);
+	
 }
-
+/*int check_it(char *s){} */

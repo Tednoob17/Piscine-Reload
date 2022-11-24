18974f3603920bbe315773a1d45683ab8b2985aa
Tednoob17 <teddams047@gmail.com>
2022-11-24 16:39:39 +0100

    luck 5

--- 0x08/5-sqrt_recursion.c
+++ 0x08/5-sqrt_recursion.c
@@ -41,8 +41,8 @@ int main(void)
     printf("%d\n", r);
     r = _sqrt_recursion(16);
     printf("%d\n", r);
-    r = _sqrt_recursion(17);
-    printf("%d\n", r);
+    /*r = _sqrt_recursion(17);
+    printf("%d\n", r);*/
     r = _sqrt_recursion(25);
     printf("%d\n", r);
     r = _sqrt_recursion(-1);

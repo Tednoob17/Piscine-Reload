172c03382e948e3e3b888d284b6547111b494f9b
Tednoob17 <teddams047@gmail.com>
2022-11-25 15:32:32 +0100

    4-pow_recursion betty check

--- 0x08/4-pow_recursion.c
+++ 0x08/4-pow_recursion.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
 
-
 /**
- *_pows_recursion - print pow of x by y
- *@x : number
- *@y : power
- *Return : POw of x by y
+ *_pow_recursion - print pow of x by y
+ *@x: number
+ *@y: power
+ *Return: Pow of x by y
  */
-int _pow_recursion(int x, int y){
+int _pow_recursion(int x, int y)
+{
 
-
-if (y == 0)
-	return (1);
-else if (x == 0 )
-	return(0);
-else if (y < 0)
-	return(-1);
-else
-	return(x *  _pow_recursion(x,y -1));
+	if (y == 0)
+		return (1);
+	else if (x == 0)
+		return (0);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
 
 /**
@@ -28,19 +27,19 @@ else
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = _pow_recursion(1, 10);
-    printf("%d\n", r);
-    r = _pow_recursion(1024, 0);
-    printf("%d\n", r);
-    r = _pow_recursion(2, 16);
-    printf("%d\n", r);
-    r = _pow_recursion(5, 2);
-    printf("%d\n", r);
-    r = _pow_recursion(5, -2);
-    printf("%d\n", r);
-    r = _pow_recursion(-5, 3);
-    printf("%d\n", r);
-    return (0);
+	r = _pow_recursion(1, 10);
+	printf("%d\n", r);
+	r = _pow_recursion(1024, 0);
+	printf("%d\n", r);
+	r = _pow_recursion(2, 16);
+	printf("%d\n", r);
+	r = _pow_recursion(5, 2);
+	printf("%d\n", r);
+	r = _pow_recursion(5, -2);
+	printf("%d\n", r);
+	r = _pow_recursion(-5, 3);
+	printf("%d\n", r);
+	return (0);
 }

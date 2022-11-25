bb6ae340190ce4f0f9d6d977a1918a893bd34e13
Tednoob17 <teddams047@gmail.com>
2022-11-25 15:49:06 +0100

    5-sqrt-recursion betty check

--- 0x08/5-sqrt_recursion.c
+++ 0x08/5-sqrt_recursion.c
@@ -3,32 +3,42 @@
 
 /**
  *_sqrt_recursion - print a square of number n
- *@n : number
- *Return : square of number n
+ *@n: number
+ *Return: square of number n
  */
-int _sqrt_recursion(int n){
-	return(verif(n, 2));
+int _sqrt_recursion(int n)
+{
+	return (verif(n, 2));
 }
 
-int verif (int n, int i)
+
+/**
+ *verif - check is the two nubers i compatible square and root
+ *@n: number
+ *@i: number who change if it's not a root
+ *Return: 0 or 1 otherwise
+ */
+int verif(int n, int i)
 {
 	if (n < 0)
-                return(-1);
-        else if (n == 0)
-                return(0);
-        else if (n == 1)
-                return (1);
-	else{
-               if (i * i == n){
-                    return(i);
-                }
-	       else{
-	       	if (i < (n / 2)){
-	       
-		    return(verif(n, i + 1));
-	       }
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+
+	else
+	{
+		if (i * i == n)
+			return (i);
+
 		else
-			return(-1);	
+		{
+			if (i < (n / 2))
+				return (verif(n, i + 1));
+
+			else
+				return (-1);
 		}
 	}
 }
@@ -39,19 +49,19 @@ int verif (int n, int i)
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = _sqrt_recursion(1);
-    printf("%d\n", r);
-    r = _sqrt_recursion(1024);
-    printf("%d\n", r);
-    r = _sqrt_recursion(16);
-    printf("%d\n", r);
-    r = _sqrt_recursion(17);
-    printf("%d\n", r);
-    r = _sqrt_recursion(25);
-    printf("%d\n", r);
-    r = _sqrt_recursion(-1);
-    printf("%d\n", r);
-    return (0);
+	r = _sqrt_recursion(1);
+	printf("%d\n", r);
+	r = _sqrt_recursion(1024);
+	printf("%d\n", r);
+	r = _sqrt_recursion(16);
+	printf("%d\n", r);
+	r = _sqrt_recursion(17);
+	printf("%d\n", r);
+	r = _sqrt_recursion(25);
+	printf("%d\n", r);
+	r = _sqrt_recursion(-1);
+	printf("%d\n", r);
+	return (0);
 }

5abd438cb7ba8c5966540f9c5c73cee2cc76302f
Tednoob17 <teddams047@gmail.com>
2022-12-07 14:52:35 +0100

    nup

--- Py-01/0x03/4-new_in_list.py
+++ Py-01/0x03/4-new_in_list.py
@@ -1,3 +1,8 @@
 #/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    list_copy=my_list.copy()
+    for i in range(0, len(my_list)):
+        if i == idx :
+            list_copy[i]=element
 
+    return list_copy

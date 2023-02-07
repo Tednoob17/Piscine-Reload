4883b6b166d4441caa8516eca3ee3ed0a5016545
Tednoob17 <teddams047@gmail.com>
2023-02-08 00:01:10 +0100

    Troncate ultime

---
+++ Py-01/0x00/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+print(str[39:66]+" ",end="")
+print(str[106:111],end="")
+print(str[:6])

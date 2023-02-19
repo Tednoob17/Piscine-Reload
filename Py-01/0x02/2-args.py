3b6d42932267acae2c664f83e6e19874c36592a4
Tednoob17 <teddams047@gmail.com>
2023-02-19 12:37:27 +0100

    mv files

--- Py-01/0x02/2-args.py
+++ Py-01/0x02/2-args.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-import sys 
+import sys
 if len(sys.argv) == 0:
 	print("0 arguments.")
-elif
+elif len(sys.argv) == 1:
+	print("1 argument :")
+	print("{} arguments:".format(argv))
+elif len(sys.argv) > 1:
+	print("{} arguments:".format(argv))
+	for i in range(len(sys.argv)):
+		print("{}: {}".format(argv[i],sys.argv[i]),end="")

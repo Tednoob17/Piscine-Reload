74da3a0947232da4dbd7275d83959bc4d5f51fce
Tednoob17 <teddams047@gmail.com>
2023-02-19 21:37:53 +0100

    multi cam

--- Py-01/0x02/100-my_calculator.py
+++ Py-01/0x02/100-my_calculator.py
@@ -1,1 +1,15 @@
-#
+#!/usr/bin/python3
+from calculator_1 import add,mul,sub,div
+import sys
+j = len(sys.argv) - 1
+l={'+' : add, '-': sub,'*':mul, '/' : div}
+if (j) < 3 :
+	print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+	sys.exit(1)
+if (sys.argv[2]) not in list(l.keys()) :
+	print("Unknown operator. Available operators: +, -, * and /")
+	sys.exit(1)
+else:
+	a = int(sys.argv[1])
+	b = int(sys.argv[3])
+	print("{} {} {} = {}".format(a , sys.argv[2] , b, l[sys.argv[2]](a, b) ))

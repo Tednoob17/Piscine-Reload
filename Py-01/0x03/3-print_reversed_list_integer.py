262bb28b2a2c9d294b8a6951410b0283c1c0f3bd
Tednoob17 <teddams047@gmail.com>
2023-02-20 02:48:07 +0100

    rewrite REVERSED

--- Py-01/0x03/3-print_reversed_list_integer.py
+++ Py-01/0x03/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    p = len(my_list) -1
-    for i in range(0, len(my_list)):
+    p = len(my_list)
+    for i in my_list :
         print("{}".format(my_list[p - i]))

211c332948dbe30e2c3b394a46abe5c5394d8afa
Tednoob17 <teddams047@gmail.com>
2023-03-06 15:57:46 +0100

    last py

--- Py-01/0x03/10-divisible_by_2.py
+++ Py-01/0x03/10-divisible_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+        my_list_copy=my_list.copy
+        for element in my_list_copy:
+                if element % 2 == 0:
+                        element = True
+                else:
+                        element = False
+
+                return my_list_copy

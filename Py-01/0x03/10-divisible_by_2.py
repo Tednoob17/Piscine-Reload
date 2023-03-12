4f738ce5dd3aa639ad56b31c5fcaf9ea2b14db49
Tednoob17 <teddams047@gmail.com>
2023-03-12 13:21:44 +0100

    multiple verif check

--- Py-01/0x03/10-divisible_by_2.py
+++ Py-01/0x03/10-divisible_by_2.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-        my_list_copy=my_list.copy
-        for element in my_list_copy:
-                if element % 2 == 0:
-                        element = True
-                else:
-                        element = False
+	"""
+	Finds all multiple of 2 in a list
+	...*
+	Parameter
+	----------
+	my_list: list
+		The list to check
 
-                return my_list_copy
+	Return:
+	A new list with True or False, depending
+         on whether the integer at the same position
+         in the original list is a multiple of 2
+	"""
+
+	my_list_copy = []
+	for i in range(len(my_list)):
+		if my_list[i] % 2 == 0:
+			my_list_copy.append(True)
+		else:
+			my_list_copy.append(False)
+
+	return my_list_copy

8b4537b551e341abb1d1f33f493e59a049419094
Ted Kouhouenou <teddams047@gmail.com>
2024-01-02 17:29:15 +0100

    2-main

--- C-00/0x0E/2-print_dog.c
+++ C-00/0x0E/2-print_dog.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include "dog.h"
+
+void print_dog(struct dog *d)
+{
+
+  if (d.name == NULL)
+    printf("(nil)")
+    else if
+      printf("Name: %s\n", d->name);
+
+  if (d.age == NULL)
+    printf("(nil)")
+    else if
+      printf("Age: %1.f\n", d->age);
+
+  if (d.owner == NULL)
+    printf("(nil)")
+    else if
+      printf("Owner: %s\n", d->owner);
+}

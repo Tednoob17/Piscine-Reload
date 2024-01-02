58b78248fa6a0f59363058380816cb39671f7e39
Ted Kouhouenou <teddams047@gmail.com>
2024-01-02 17:35:20 +0100

    2-print_dog finish

--- C-00/0x0E/2-print_dog.c
+++ C-00/0x0E/2-print_dog.c
@@ -4,18 +4,19 @@
 void print_dog(struct dog *d)
 {
 
-  if (d.name == NULL)
-    printf("(nil)")
-    else if
-      printf("Name: %s\n", d->name);
+  if (d->name == NULL)
+    printf("(nil)");
+  else 
+    printf("Name: %s\n", d->name);
 
-  if (d.age == NULL)
-    printf("(nil)")
-    else if
-      printf("Age: %1.f\n", d->age);
+  if (d->age <= 0)
+    printf("(nil)");
+  else 
+    printf("Age: %.6f\n", d->age);
 
-  if (d.owner == NULL)
-    printf("(nil)")
-    else if
-      printf("Owner: %s\n", d->owner);
+  if (d->owner == NULL)
+    printf("(nil)");
+  else 
+    printf("Owner: %s\n", d->owner);
+
 }
